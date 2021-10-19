61780dd0c51f3a5064f5ca1309ae9f4d14f34ece
Rocio-hub <roci0055@easv365.dk>
2021-10-19 18:09:18 +0200

    login xml

--- app/src/main/java/com/easv/esbjergensemblescanningapp/Activities/LoginActivity.kt
+++ app/src/main/java/com/easv/esbjergensemblescanningapp/Activities/LoginActivity.kt
@@ -1,4 +1,13 @@
 package com.easv.esbjergensemblescanningapp.Activities
 
-class LoginActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.easv.esbjergensemblescanningapp.R
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
 }
